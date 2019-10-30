0fcc4c9065bf0ae71cb7e39f54d6770278bf5d6f
Jacob Parry <japadm@divvypay.com>
2019-10-30 12:15:15 -0600

    key value pair

--- apps/legion/lib/tasks.ex
+++ apps/legion/lib/tasks.ex
@@ -14,7 +14,43 @@ defmodule Legion.Tasks do
     Args: []
   """
 
-  def new_task() do
-    # something
+  def start() do
+    Task.start(fn -> "Heyo" end)
+  end
+
+  def start_link() do
+    Task.start_link(fn -> "Heyo" end)
+  end
+
+  def start_bad() do
+    Task.start(fn -> raise "oopsie" end)
+  end
+
+  def start_link_bad() do
+    Task.start_link(fn -> raise "oopsie" end)
+  end
+
+  def task_store_state do
+    Task.start_link(fn -> loop(%{}) end)
+  end
+
+  defp loop(map) do
+    receive do
+      {:get, key, caller} ->
+        send(caller, Map.get(map, key))
+        loop(map)
+
+      {:put, key, value} ->
+        new_map = Map.put(map, key, value)
+        loop(new_map)
+    end
+  end
+
+  def give_process_a_name(pid, name) do
+    Process.register(pid, name)
+  end
+
+  def find_named_processes() do
+    Process.registered()
   end
 end
